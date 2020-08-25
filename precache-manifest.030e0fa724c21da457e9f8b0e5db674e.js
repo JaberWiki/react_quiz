761ab495b8394118c80bd2cf7d881dedd73543a8
hmjaber <55241455+hmjaber@users.noreply.github.com>
2020-08-25 19:32:21 +0600

    Add files via upload

---
+++ precache-manifest.030e0fa724c21da457e9f8b0e5db674e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755cd66f555806a526cd1a58e1de5593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5703cc60403cd6f9ef2",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "dce5557e948fca640e7d",
+    "url": "/static/js/2.593dcfa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.593dcfa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5703cc60403cd6f9ef2",
+    "url": "/static/js/main.c5480c88.chunk.js"
+  },
+  {
+    "revision": "9fd988a7c3804845b4c8",
+    "url": "/static/js/runtime-main.64cdb813.js"
+  }
+]);
